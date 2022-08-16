9edcc35f88aae38e33972de3f0aa0196923e67b3
Thays Gorska <thays.gorska@gmail.com>
2022-08-16 15:35:03 -0300

    Alterando o formato do exercício 4

--- TerceiroProjeto/src/main/kotlin/Exercicio4.kt
+++ TerceiroProjeto/src/main/kotlin/Exercicio4.kt
@@ -1,3 +1,5 @@
+import kotlin.math.pow
+
 fun main(){
 
     /*Faça um programa em que permita a entrada de um número qualquer e exiba se estenúmero é par ou ímpar.
@@ -5,13 +7,13 @@ Se for par exiba também a raiz quadrada do mesmo; se for ímpar exiba o número
 
     print("Digite um número para saber se é par ou ímpar: ")
     val num = readln().toInt()
-    val pot = num * num
+    val pot = num.toDouble().pow(2)
 
     if (num % 2 == 0) {
         val raiz = Math.sqrt(num.toDouble())
-        println("O número digitado é: PAR e sua raiz quadrada é: " + raiz)
+        println("O número digitado é: PAR e sua raiz quadrada é: ${"%.2f".format(raiz)}")
     }
     else{
-        println("O número digitado é: ÍMPAR e seu quadrado é: " + pot)
+        println("O número digitado é: ÍMPAR e seu quadrado é: $pot")
     }
 }
